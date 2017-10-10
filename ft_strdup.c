6dd28d2093aa5932a1955a8ba658d12e262782ea
Iurie Gornea <igornea@e1z2r4p22.42.us.org>
2017-10-09 20:52:04 -0700

    pls

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: igornea <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/09/25 21:41:00 by igornea           #+#    #+#             */
+/*   Updated: 2017/10/03 19:21:05 by igornea          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*dup;
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if ((dup = malloc(sizeof(char) * (i + 1))) == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
